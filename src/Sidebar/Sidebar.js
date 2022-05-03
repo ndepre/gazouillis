4ab6c408c6f0f1c41fa7c093bf665336c88b7bae
ndepre <73833520+ndepre@users.noreply.github.com>
2022-05-03 23:55:11 +0200

    :rewind: "Features/profile (#5)" (#6)" (#7)
    This reverts commit 2bd4d0b6d7ba1bc9d852feebee58f1afac0aa7b5.

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -3,29 +3,18 @@ import "./Sidebar.css";
 import TwitterIcon from '@mui/icons-material/Twitter';
 import SidebarOption from "./SidebarOption";
 import HomeIcon from "@mui/icons-material/Home";
-import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
-import MailOutlineIcon from "@mui/icons-material/MailOutline";
 import PermIdentityIcon from "@mui/icons-material/PermIdentity";
-import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
 
 function Sidebar() {
 
-  return (
-    <div className="sidebar">
-      
-      <TwitterIcon className="sidebar__twitterIcon" />
-      
-        <Link to="/Home"><SidebarOption Icon={HomeIcon} text="Home" /></Link>
-        <Link to="/Notifications"><SidebarOption Icon={NotificationsNoneIcon} text="Notifications" /></Link>
-        <Link to="/Messages"><SidebarOption Icon={MailOutlineIcon} text="Messages" /></Link>
-        <Link to="/Login"><SidebarOption Icon={PermIdentityIcon} text="Profile" /></Link>
-
-        <Button variant="outlined" className="sidebar__tweet" fullWidth>
-          Tweet
-        </Button>
-    </div>
-  );
+	return (
+		<div className="sidebar">
+			<Link to="/"><TwitterIcon className="sidebar__twitterIcon" /></Link>	
+			<Link to="/Home"><SidebarOption Icon={HomeIcon} text="Home" /></Link>
+			<Link to="/Profile"><SidebarOption Icon={PermIdentityIcon} text="Profile" /></Link>
+		</div>
+	);
 }
 
 export default Sidebar;
