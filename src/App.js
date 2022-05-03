f40d4a1fa53c397359c898586912ca8dc990ed20
Frixix <ndepre11@gmail.com>
2022-05-03 20:12:46 +0200

    :sparkles: add router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import "./App.css";
+import Login from './Login';
+import {Route, Routes } from "react-router-dom";
 import Sidebar from './Sidebar/Sidebar';
 import Feed from './Feed/Feed';
-import Widgets from "./Leftbar/Widgets";
+import Widgets from './Leftbar/Widgets';
 
 function App() {
   return (
     <div className="app">
       <Sidebar />
-      <Feed />
-      <Widgets />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/home" element={<Feed />} />
+        <Route path="/Login" element={<Login />} />
+      </Routes>
+      <Widgets/>
     </div>
   );
 }
--- src/Sidebar/SidebarOption.js
+++ src/Sidebar/SidebarOption.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./SidebarOption.css";
 
 function SidebarOption({ active, text, Icon }) {
+
   return (
     <div className={`sidebarOption ${active && "sidebarOption--active"}`}>
       <Icon />
@@ -9,5 +10,7 @@ function SidebarOption({ active, text, Icon }) {
     </div>
   );
 }
+
+
 
 export default SidebarOption;
